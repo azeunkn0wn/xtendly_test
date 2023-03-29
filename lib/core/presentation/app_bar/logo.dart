4f03c29112607b4b9fe9450bfb7b1b91f792f4b9
azeunkn0wn <azeunkn0wn@gmail.com>
2023-03-29 19:03:24 +0800

    update added onTap and snackbar for Logo

--- lib/core/presentation/app_bar/logo.dart
+++ lib/core/presentation/app_bar/logo.dart
@@ -7,16 +7,24 @@ class Logo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox.fromSize(
-      size: const Size.fromRadius(25),
-      child: CircleAvatar(
-        backgroundColor: const Color(0xFFD9D9D9),
-        child: Text(
-          'LOGO',
-          style: TextStyle(
-            color: Theme.of(context).colorScheme.primary,
-            fontSize: 10,
-            fontWeight: FontWeight.w500,
+    return InkWell(
+      onTap: () {
+        const snackBar = SnackBar(
+          content: Text('Logo'),
+        );
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      },
+      child: SizedBox.fromSize(
+        size: const Size.fromRadius(25),
+        child: CircleAvatar(
+          backgroundColor: const Color(0xFFD9D9D9),
+          child: Text(
+            'LOGO',
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+              fontSize: 10,
+              fontWeight: FontWeight.w500,
+            ),
           ),
         ),
       ),
