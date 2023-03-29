2bd694ec4a6631d7fdce178eef98a6078038f95a
azeunkn0wn <azeunkn0wn@gmail.com>
2023-03-29 18:55:34 +0800

    style remove padding in ActionButtons

--- lib/core/presentation/app_bar/action_buttons.dart
+++ lib/core/presentation/app_bar/action_buttons.dart
@@ -9,63 +9,71 @@ class ActionButtons extends StatelessWidget {
   const ActionButtons({
     super.key,
   });
+  static const badgeCount = 0;
 
   @override
   Widget build(BuildContext context) {
     const double dividerPadding = 3;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 48),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          const ResponsiveVisibility(
-            hiddenWhen: [Condition.smallerThan(name: DESKTOP)],
-            child: Flexible(
-              child: Padding(
-                padding: EdgeInsets.only(left: dividerPadding, right: 17),
-                child: SearchField(),
-              ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        const ResponsiveVisibility(
+          hiddenWhen: [Condition.smallerThan(name: DESKTOP)],
+          child: Flexible(
+            child: Padding(
+              padding: EdgeInsets.only(left: dividerPadding, right: 17),
+              child: SearchField(),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: dividerPadding),
-            child: badges.Badge(
-              badgeContent: const Text(
-                '0',
-                style: TextStyle(fontSize: 7),
-              ),
-              position: badges.BadgePosition.bottomEnd(bottom: -5, end: -5),
-              badgeStyle: const badges.BadgeStyle(
-                padding: EdgeInsets.all(4),
-                badgeColor: Color(0xFFD9D9D9),
-              ),
-              child: InkWell(
-                onTap: () {},
-                child: SizedBox(
-                  height: 28,
-                  width: 28,
-                  child: SvgPicture.asset(
-                    'icons/unknown.svg',
-                  ),
-                ),
-              ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: dividerPadding),
+          child: badges.Badge(
+            badgeContent: Text(
+              badgeCount.toString(),
+              style: const TextStyle(fontSize: 7),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
+            position: badges.BadgePosition.bottomEnd(bottom: -5, end: -5),
+            badgeStyle: const badges.BadgeStyle(
+              padding: EdgeInsets.all(4),
+              badgeColor: Color(0xFFD9D9D9),
+            ),
             child: InkWell(
-              onTap: () {},
+              onTap: () {
+                const snackBar = SnackBar(
+                  content: Text('Cart? button with badge count: $badgeCount'),
+                );
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              },
               child: SizedBox(
                 height: 28,
                 width: 28,
                 child: SvgPicture.asset(
-                  'icons/star_1.svg',
+                  'icons/unknown.svg',
                 ),
               ),
             ),
           ),
-        ],
-      ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: InkWell(
+            onTap: () {
+              const snackBar = SnackBar(
+                content: Text('Star button'),
+              );
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            },
+            child: SizedBox(
+              height: 28,
+              width: 28,
+              child: SvgPicture.asset(
+                'icons/star_1.svg',
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
