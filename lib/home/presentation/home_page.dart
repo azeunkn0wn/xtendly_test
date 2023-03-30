079e4b4ee5345d770112ca56c0010c8f075cf606
azeunkn0wn <azeunkn0wn@gmail.com>
2023-03-30 17:31:35 +0800

    refactor added footer

--- lib/home/presentation/home_page.dart
+++ lib/home/presentation/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 import 'package:xtendly_test/core/presentation/app_bar/app_bar.dart';
+import 'package:xtendly_test/core/presentation/footer/footer.dart';
 import 'package:xtendly_test/core/presentation/top_text_button.dart';
 import 'package:xtendly_test/home/application/items_notifier.dart';
 import 'package:xtendly_test/home/shared/provider.dart';
@@ -54,27 +55,45 @@ class BodyContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Flexible(
-          child: state.maybeMap(
-            orElse: () => const Center(
-              child: CircularProgressIndicator(),
-            ),
-            loadSuccess: (state) => ListView.builder(
-              shrinkWrap: true,
-              itemCount: state.items.length,
-              itemBuilder: (context, index) {
-                final item = state.items[index];
-                return ListTile(
-                  title: Text(item!.name),
-                  subtitle: Text(item.image),
-                );
-              },
-            ),
-          ),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ItemsGrid(state: state),
+          const Footer(),
+        ],
+      ),
+    );
+  }
+}
+
+class ItemsGrid extends StatelessWidget {
+  const ItemsGrid({
+    super.key,
+    required this.state,
+  });
+
+  final ItemsState state;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: state.maybeMap(
+        orElse: () => const Center(
+          child: CircularProgressIndicator(),
         ),
-      ],
+        loadSuccess: (state) => ListView.builder(
+          shrinkWrap: true,
+          itemCount: state.items.length,
+          itemBuilder: (context, index) {
+            final item = state.items[index];
+            return ListTile(
+              title: Text(item!.name),
+              subtitle: Text(item.image),
+            );
+          },
+        ),
+      ),
     );
   }
 }
