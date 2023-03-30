b47b47592dd1cadf5a778b091a371a66e67a71fa
azeunkn0wn <azeunkn0wn@gmail.com>
2023-03-30 22:27:24 +0800

    feat responsive header, added mobile layout

--- lib/home/presentation/header.dart
+++ lib/home/presentation/header.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 class HeaderWidget extends StatefulWidget {
   const HeaderWidget({
@@ -12,53 +13,62 @@ class HeaderWidget extends StatefulWidget {
 class _HeaderWidgetState extends State<HeaderWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 1024,
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/images/5.png'),
-          fit: BoxFit.cover,
+    final isBigScreen = ResponsiveWrapper.of(context).isLargerThan(TABLET);
+    return AspectRatio(
+      aspectRatio: isBigScreen ? 1440 / 1024 : 375 / 812,
+      child: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/5.png'),
+            fit: BoxFit.cover,
+          ),
         ),
-      ),
-      child: LayoutBuilder(
-        builder: (context, constraints) {
-          final height = constraints.maxHeight;
-          final width = constraints.maxWidth;
-          return Stack(
-            children: [
-              Positioned(
-                top: height * 0.249,
-                left: 0,
-                width: width * 0.264,
-                child: const Image(
-                  fit: BoxFit.cover,
-                  image: AssetImage('assets/images/3.png'),
-                ),
-              ),
-              Positioned(
-                top: 215,
-                right: 0,
-                width: width * 0.502,
-                child: const Image(
-                  fit: BoxFit.cover,
-                  image: AssetImage('assets/images/4.png'),
-                ),
-              ),
-              Positioned(
-                top: height * 0.282,
-                left: width * 0.246,
-                width: width * 0.264,
-                child: ConstrainedBox(
-                  constraints: const BoxConstraints(),
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            final height = constraints.maxHeight;
+            final width = constraints.maxWidth;
+            return Stack(
+              children: [
+                AnimatedPositioned(
+                  duration: const Duration(milliseconds: 200),
+                  top: isBigScreen ? height * 0.249 : height * 0.612,
+                  left: isBigScreen ? 0 : width * 0.464,
+                  width: isBigScreen ? width * 0.264 : null,
+                  height: isBigScreen ? null : height * 0.324,
                   child: const Image(
                     fit: BoxFit.cover,
-                    image: AssetImage('assets/images/2.png'),
+                    image: AssetImage('assets/images/3.png'),
                   ),
                 ),
-              ),
-            ],
-          );
-        },
+                AnimatedPositioned(
+                  duration: const Duration(milliseconds: 200),
+                  top: isBigScreen ? height * 0.210 : height * 0.138,
+                  right: 0,
+                  width: isBigScreen ? width * 0.502 : null,
+                  height: isBigScreen ? null : height * 0.243,
+                  child: const Image(
+                    fit: BoxFit.cover,
+                    image: AssetImage('assets/images/4.png'),
+                  ),
+                ),
+                AnimatedPositioned(
+                  duration: const Duration(milliseconds: 200),
+                  top: isBigScreen ? height * 0.300 : height * 0.362,
+                  left: isBigScreen ? width * 0.246 : width * 0.05,
+                  width: isBigScreen ? width * 0.264 : null,
+                  height: isBigScreen ? null : height * 0.362,
+                  child: ConstrainedBox(
+                    constraints: const BoxConstraints(),
+                    child: const Image(
+                      fit: BoxFit.cover,
+                      image: AssetImage('assets/images/2.png'),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
