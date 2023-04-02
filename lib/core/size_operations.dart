301a3358c603c2d7f08f5f0a138298aaec60a1a5
azeunkn0wn <azeunkn0wn@gmail.com>
2023-04-02 16:38:40 +0800

    chore clean up unused function

--- lib/core/size_operations.dart
+++ lib/core/size_operations.dart
@@ -4,27 +4,15 @@ import 'package:responsive_framework/responsive_framework.dart';
 Size multiplySize(Size size1, Size size2) {
   final width = size1.width * size2.width;
   final height = size1.height * size2.height;
-  // print('multiplying');
+
   return Size(width, height);
 }
 
 Size divideSize(Size size1, Size size2) {
   final width = size1.width / size2.width;
   final height = size1.height / size2.height;
-  // print('dividing');
-  return Size(width, height);
-}
 
-/// scale size using reference sizes from figma design or the like
-Size scaleByReference(Size parentSize, Size refWidgetSize, Size refParentSize) {
-  // print('scaling');
-  return multiplySize(
-    parentSize,
-    divideSize(
-      refWidgetSize,
-      refParentSize,
-    ),
-  );
+  return Size(width, height);
 }
 
 /// shortcut for ResponsiveValue
